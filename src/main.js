e817f83f19dfc09f712ada0e61146ef830c6de46
MariselaSA <mariselaestudy@gmail.com>
2024-10-04 10:58:58 -0500

    filtro por generos

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 //Importar funciones para poder mostrar en pantalla principal
 //import { sortData } from './dataFunctions.js';
-//import { filterData } from './dataFunctions.js';
+import { filterData } from './dataFunctions.js';
 import { renderMovies } from './view.js';
 import data from './data/dataset.js';
+console.log(Array.isArray(data), data);
 const lista = document.querySelector("#root>ul");
 const html = renderMovies(data);
 lista.innerHTML = html;
 console.log(lista);
 const genero=document.querySelector("#genero");
-const ordenar=document.querySelector("#ordenar");
+//const ordenar=document.querySelector("#ordenar");
 
 genero.addEventListener('change', function (event) {
-    const filterBy = "departamento";
+   const filterBy = "genero";
     const selectedValue = event.target.value; // Obtiene el valor seleccionado
-    const filterData = filterData(data, filterBy, selectedValue); // Filtra los datos
-    lista.innerHTML = renderItems(filterData); // Renderiza los datos filtrados
+    const filteredData = filterData(data, filterBy, selectedValue); // Filtra los datos
+    console.log(filteredData); 
+    lista.innerHTML = renderMovies(filteredData); // Renderiza los datos filtrados
 })
--- src/view.js
+++ src/view.js
@@ -3,8 +3,8 @@ export const renderMovies = (data) => {
           <li class="li" itemtype="tarjeta" itemscope>
         <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="${movie.title}">
 
-        <p class=title>${movie.title}<p>      
-        <p class=estreno>AÑO DE ESTRENO:${movie.release_date}</p>
+        <p class="title">${movie.title}</p>      
+        <p class="estreno">AÑO DE ESTRENO:>${movie.release_date}</p>
       </li>    
     `).join('');
   };
